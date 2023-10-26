baa19a9bd50e676ffafa78c0509f463ea9dfa850
Alexander DeRieux <alexander.derieux@gmail.com>
2023-10-26 17:54:25 -0400

    Adds `CircuitGeneratorFunctionType`

--- eqmarl/types.py
+++ eqmarl/types.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Any, Callable
+from typing import Any, Callable, Iterable
 import cirq
 import sympy
 
@@ -12,6 +12,7 @@ QubitGateFunctionType = Callable[[QubitType], Any]
 VariationalCircuitFunctionType = Callable[[QubitListType, list[list[float]]], Any]
 EncodingCircuitFunctionType = Callable[[QubitListType, list[float]], Any]
 EntanglingCircuitFunctionType = Callable[[QubitListType], Any]
+CircuitGeneratorFunctionType = Callable[[], Iterable[Any]] # Generator function that yields gate operations that define a given quantum circuit.
 
 SymbolType = str | sympy.Symbol
 SymbolListType = list[SymbolType]

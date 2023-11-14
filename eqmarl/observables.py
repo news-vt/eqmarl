a38fe2fd8df40943cfa34920d9e1ca652fc37a78
Alexander DeRieux <alexander.derieux@gmail.com>
2023-11-14 13:14:43 -0500

    New observables API
    Adds functions to help create individual Pauli observables, Pauli tensor product observables, and weighted Pauli observables

--- eqmarl/observables.py
+++ eqmarl/observables.py
@@ -1,4 +1,8 @@
 import pennylane as qml
+# from pennylane import numpy as np
+import numpy as np
+from numpy.typing import NDArray
+from pennylane.operation import Operation
 from functools import reduce
 
 
@@ -19,3 +23,48 @@ def IndividualPauliZ(wires) -> list:
         obs = qml.PauliZ(w)
         all_obs.append(obs)
     return all_obs
+
+
+
+def PauliObservables(wires: list, op: Operation = qml.PauliZ) -> NDArray:
+    """Creates a list of observables by applying the specified Pauli operator (defaults to PauliZ) on each wire."""
+    return np.asarray([op(wires=w) for w in wires])
+
+
+def TensorObservables(obs: NDArray) -> NDArray:
+    """Creates a single observable via tensor product of a list of observables.
+    
+    Input observables can either be 1-dimensional list, or nested lists.
+    
+    Returns a single 1-dimensional list containing tensor product of all observables. If input is a nested list, then each returned entry is the tensor product of the corresponding nested list.
+    """
+    obs = np.asarray(obs) # Ensure type is numpy array.
+    if len(obs.shape) > 1:
+        obs = [TensorObservables(o) for o in obs]
+    else:
+        obs = reduce((lambda a, b: a @ b), obs) # Reduce to tensor product.
+    return np.asarray(obs).reshape((-1,)) # Ensure final output is 1-dimensional. 
+
+
+def WeightedObservables(weights: NDArray, obs: NDArray) -> NDArray:
+    """Multiplies a list/matrix of weights by a list/matrix of observables.
+    
+    Creates a weighted Hamiltonian for each observable.
+    
+    Returns a 1-dimensional list of weighted observables.
+    """
+    obs = np.asarray(obs) # Ensure type is numpy array.
+    if len(obs.shape) != 2: # Ensures shape is (n_obs, 1)
+        obs = np.expand_dims(obs.reshape((-1,)), axis=-1)
+    res = weights * obs
+    return res.reshape((-1,)) # Ensure final output is 1-dimensional.
+
+
+def AlternatingWeightedObservables(n_reps: int, obs: list, weight: float = -1.) -> list:
+    """Duplicates observables in alternating fashion with $(-1)^i * O$ weight where $i$ is the repetition index.
+    
+    Returns a 1-dimensional list of weighted observables.
+    """
+    weights = np.asarray([weight**i for i in range(n_reps)])
+    res = WeightedObservables(weights=weights, obs=obs)
+    return res.reshape((-1,)) # Ensure final output is 1-dimensional.

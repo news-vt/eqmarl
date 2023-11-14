7ed64b97e3dfdf1eddffad904a5ea3ffb4589ea9
Alexander DeRieux <alexander.derieux@gmail.com>
2023-11-13 19:57:40 -0500

    Moved state preparation code into `ops` module

--- eqmarl/ops.py
+++ eqmarl/ops.py
@@ -7,6 +7,58 @@ from pennylane import numpy as np
 from .tools import flatten_to_operations
 
 
+## Functions to create various entangled input states.
+
+def entangle_agents_phi_plus(wires: WireListType, d: int, n: int):
+    """Entangles via $\\Phi^+$."""
+    for i in range(d):
+        qml.Hadamard(wires=wires[i])
+        for j in range(n-1):
+            qml.CNOT(wires=[wires[j*d + i], wires[(j+1)*d + i]])
+
+
+def entangle_agents_phi_minus(wires: WireListType, d: int, n: int):
+    """Entangles via $\\Phi^-$."""
+    for i in range(d):
+        qml.PauliX(wires=wires[i])
+        qml.Hadamard(wires=wires[i])
+        for j in range(n-1):
+            qml.CNOT(wires=[wires[j*d + i], wires[(j+1)*d + i]])
+
+
+def entangle_agents_psi_plus(wires: WireListType, d: int, n: int):
+    """Entangles via $\\Psi^+$."""
+    for i in range(d):
+        qml.Hadamard(wires=wires[i])
+        for j in range(n-1):
+            qml.PauliX(wires=wires[(j+1)*d + i])
+            qml.CNOT(wires=[wires[j*d + i], wires[(j+1)*d + i]])
+
+
+def entangle_agents_psi_minus(wires: WireListType, d: int, n: int):
+    """Entangles via $\\Psi^-$."""
+    for i in range(d):
+        qml.PauliX(wires=wires[i])
+        qml.Hadamard(wires=wires[i])
+        for j in range(n-1):
+            qml.PauliX(wires=wires[(j+1)*d + i])
+            qml.CNOT(wires=[wires[j*d + i], wires[(j+1)*d + i]])
+
+
+def prepare_state_from_state_vector(
+    wires: WireListType,
+    state: np.ndarray,
+    ):
+    """Prepares an arbitrary state from state-vector representation.
+    
+    This function wraps the built-in `qml.StatePrep` to accommodate a list of qubits. 
+    """
+    qml.StatePrep(state=state, wires=wires)
+    # yield cirq.StatePreparationChannel(target_state=target_state, name=name)(*qubits)
+
+
+## Variational / Encoding / Entangling operations
+
 
 def circular_entangling_layer(
     wires: WireListType,

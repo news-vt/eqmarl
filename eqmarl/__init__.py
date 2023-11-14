310a2837c5a1bd27a59fb69206b743ee031e813a
Alexander DeRieux <alexander.derieux@gmail.com>
2023-11-13 19:54:32 -0500

    Now imports `ops`

--- eqmarl/__init__.py
+++ eqmarl/__init__.py
@@ -3,5 +3,6 @@ from .experiment import *
 from .tools import *
 from .types import *
 from .parameter_resolvers import *
-from .layers import *
-from .models import *
+from .ops import *
+# from .layers import *
+# from .models import *

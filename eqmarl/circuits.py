0a68a1c83009197d4366d89a376c8c1f7271f33c
Alexander DeRieux <alexander.derieux@gmail.com>
2024-01-24 14:49:07 -0500

    Adds variational and encoding circuit for single-agent case

--- eqmarl/circuits.py
+++ eqmarl/circuits.py
@@ -5,6 +5,64 @@ import cirq
 from .ops import *
 
 
+def generate_variational_encoding_circuit(
+    qubits: list,
+    d_qubits: int, # Number of qubits per partition.
+    n_layers: int,
+    decompose: bool = False,
+    variational_layer_cls: ParameterizedOperationGate = VariationalRotationLayer,
+    encoding_layer_cls: ParameterizedOperationGate = EncodingLayer,
+    ) -> tuple[cirq.Circuit, tuple[np.ndarray,...]]:
+    """Parameterized variational and encoding circuit.
+    """
+    
+    shape_var = variational_layer_cls.get_shape(d_qubits)
+    shape_enc = encoding_layer_cls.get_shape(d_qubits)
+    
+    ### Define weights for circuit.
+    #
+    ## Variational shape
+    theta_var = sympy.symbols(f'var(0:{n_layers+1})_' + '_'.join(f'(0:{x})' for x in shape_var))
+    theta_var = np.asarray(theta_var).reshape((n_layers+1, *shape_var))
+    ## Encoding shape
+    theta_enc = sympy.symbols(f'enc(0:{n_layers})_' + '_'.join(f'(0:{x})' for x in shape_enc))
+    theta_enc = np.asarray(theta_enc).reshape((n_layers, *shape_enc))
+    
+    # Build the circuit.
+    ops = []
+
+    # Build circuit in layers.
+    for l in range(n_layers):
+        # Variational layer.
+        ops.append(
+            variational_layer_cls(theta_var[l], name=f'v{l}')(*qubits)
+        )
+        
+        # Entangling layer.
+        ops.append(
+            circular_entangling_layer(qubits)
+        )
+        
+        # Encoding layer.
+        ops.append(
+            encoding_layer_cls(theta_enc[l], name=f'e{l}')(*qubits)
+        )
+    # Last variational layer at the end.
+    ops.append(
+        variational_layer_cls(theta_var[l+1], name=f'v{l+1}')(*qubits)
+    )
+    
+    # Decompose circuit into minimal gate representation.
+    # This is required when custom gates are implemented for use with TensorFlowQuantum
+    if decompose:
+        ops = [cirq.decompose(o) for o in cirq.ops.flatten_to_ops(ops)]
+
+    circuit = cirq.Circuit(ops)
+
+    return circuit, (theta_var, theta_enc)
+
+
+
 def generate_partite_variational_encoding_circuit(
     qubits: list,
     n_parts: int, # Number of partitions.
@@ -14,6 +72,8 @@ def generate_partite_variational_encoding_circuit(
     variational_layer_cls: ParameterizedOperationGate = VariationalRotationLayer,
     encoding_layer_cls: ParameterizedOperationGate = EncodingLayer,
     ) -> tuple[cirq.Circuit, tuple[np.ndarray,...]]:
+    """eQMARL variant of parameterized variational and encoding circuit.
+    """
     
     shape_var = variational_layer_cls.get_shape(d_qubits)
     shape_enc = encoding_layer_cls.get_shape(d_qubits)
@@ -28,7 +88,6 @@ def generate_partite_variational_encoding_circuit(
     theta_enc = np.asarray(theta_enc).reshape((n_parts, n_layers, *shape_enc))
     
     # Build the circuit.
-    # circuit = cirq.Circuit()
     ops = []
     
     # Add GHZ entangling layer at the start.

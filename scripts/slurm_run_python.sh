c5b923397e7b27a2a55703341f2717b0650fc52a
Alexander DeRieux <alexander.derieux@gmail.com>
2024-04-18 13:03:47 -0400

    New SLURM runner script for Python

---
+++ scripts/slurm_run_python.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=p100_normal_q
+#SBATCH --nodes=1
+#SBATCH --ntasks 4
+#SBATCH --cpus-per-task 4
+#SBATCH --time=48:00:00
+#SBATCH --account="bigdata"
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=${USER}@vt.edu
+
+# Load modules.
+echo "Loading modules"
+module reset >/dev/null 2>&1
+module load Anaconda3 >/dev/null 2>&1
+
+# Build anaconda environment name from SLURM allocation.
+CONDA_ENV_NAME="eqmarl"
+
+# Initialize the shell to use Anaconda.
+eval "$(conda shell.bash hook)"
+
+# Activate Anaconda environment.
+conda activate ${CONDA_ENV_NAME}
+
+# Propagate arguments to Python and run.
+python $@

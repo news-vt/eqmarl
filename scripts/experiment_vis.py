f98757adffed04be06e1afde9c581f9bd8a5d6b2
Alexander DeRieux <alexander.derieux@gmail.com>
2024-04-25 11:44:39 -0400

    Experiment visualizer now supports multiple input files and plot arguments

--- scripts/experiment_vis.py
+++ scripts/experiment_vis.py
@@ -31,22 +31,74 @@ def get_opts() -> argparse.Namespace:
     parser.add_argument('config',
         help='Experiment config file in YAML format.',
         )
-    parser.add_argument('dir',
-        help='Session directory containing the metrics files to visualize.',
+    # parser.add_argument('dir',
+    #     help='Session directory containing the metrics files to visualize.',
+    #     )
+    parser.add_argument('files',
+        nargs='+',
+        help='Metrics files to visualize.',
+        )
+    parser.add_argument('-s', '--sort',
+        action='store_true',
+        help='Performs a lexical sort of the filepaths prior to processing.',
         )
     args = parser.parse_args()
     return args
 
 
-
-def plot_with_errorbar(ax, data, axis):
+# method in ['std', 'minmax']
+def plot_with_errorbar(ax, data, axis, error_method: str = 'std', plot_data: str = 'mean'):
     
-    score_mean = np.mean(data, axis=0) # (3000,)
-    score_std = np.std(data, axis=0)# (3000,)
-    x = np.arange(data.shape[-1])
+    ###
+    # Method to highlight the main plot data.
+    ###
+    y = data
     
-    ax.plot(x, score_mean, 'b-', linewidth=0.2)
-    ax.fill_between(x, score_mean - score_std, score_mean+score_std, color='b', alpha=0.2, linewidth=0.2)
+    # Plots the average run value at each epoch.
+    if plot_data == 'mean':
+        y = np.mean(data, axis=0) # (3000,)
+    
+    # Plots the run with the maximum average value over the last `n` epochs.
+    elif plot_data.startswith('max-'):
+        n = int(plot_data.split('-')[-1])
+        avg_last_n = np.mean(data[:,-n:], axis=1)
+        idx_max = np.argmax(avg_last_n)
+        y = data[idx_max] # Plot data is the index of the max.
+        
+    # Plots the run with the minimum average value over the last `n` epochs.
+    elif plot_data.startswith('min-'):
+        n = int(plot_data.split('-')[-1])
+        avg_last_n = np.mean(data[:,-n:], axis=1)
+        idx_min = np.argmin(avg_last_n)
+        y = data[idx_min] # Plot data is the index of the min.
+    
+    else:
+        raise ValueError(f"Unsupported plot highlight {plot_data}")
+    
+    ###
+    # Method to produce the shaded error regions.
+    ###
+    
+    # Shaded region is +/- standard deviation from the designated `y`-value.
+    if error_method.startswith('std'):
+        y_std = np.std(data, axis=0)# (3000,)
+        n = 1 # Default is 1 std above/below the data.
+        if '-' in error_method: # Pull `n` value from method type.
+            n = int(error_method.split('-')[-1])
+        x = np.arange(data.shape[-1])
+        ax.plot(x, y, 'b-', linewidth=0.2)
+        ax.fill_between(x, y - y_std, y+y_std, color='b', alpha=0.2, linewidth=0.2)
+    
+    # Shaded region is minimum/maximum values at each epoch.
+    elif error_method == 'minmax':
+        y_min = np.min(data, axis=0) # (3000,)
+        y_max = np.max(data, axis=0)# (3000,)
+        x = np.arange(data.shape[-1])
+        ax.plot(x, y, 'b-', linewidth=0.2)
+        ax.fill_between(x, y_min, y_max, color='b', alpha=0.2, linewidth=0.2)
+    
+    else:
+        raise ValueError(f"Unsupported error method {error_method}")
 
 
 
@@ -55,68 +107,49 @@ def plot_with_errorbar(ax, data, axis):
 
 
 
-def main(config: dict, dir: str):
+def main(name, config: dict, files: list[str], flag_sort_files: bool):
     
     # session_dir = config['experiment']['session_dir']
     
     exp = experiment_runner.load_experiment(config)
     algo = exp['algorithm']
     
-    print(dir)
-    files = sorted([f for f in Path(dir).iterdir() if f.is_file()])
-    print(files)
+    # Collect filepaths and sort if necessary.
+    files = [Path(f) for f in files]
+    if flag_sort_files:
+        files = sorted(files)
     
     
-    
+    # Load metric data.
     session_reward_history = []
     session_metrics_history = []
-    
     for f in files:
         reward_history, metrics_history = algo.load_train_results(str(f))
-        
         session_reward_history.append(reward_history)
         session_metrics_history.append(metrics_history)
-        # session_history.append((reward_history, metrics_history))
-        
-        # reward_history = np.array(reward_history)
-        # score = np.sum(reward_history, axis=-1)
-        
-        # print(f"{reward_history.shape=}")
-        # plt.plot(score, linewidth=.2)
-        # plt.title('Score')
-        # plt.show()
-        
+
+    # Reshape to proper matrix.
     session_reward_history = session_reward_history
     session_reward_history = np.array(session_reward_history)
-    print(f"{session_reward_history.shape=}")
 
-    # score = np.sum(session_reward_history, axis=-1)
-    # print(f"{score.shape=}")
-    
-    
-    # score_mean = np.mean(score, axis=0) # (3000,)
-    # score_std = np.std(score, axis=0)# (3000,)
-    # x = np.arange(score.shape[-1])
-    
-    # mosaic = [
-    #     key for key in ['score'] + list(metrics_history.keys())
-    # ]
-    # mosaic = np.array(mosaic).reshape((3,2))
-    
+    # Create figure and axes using info from config file.
     config_plot = config['experiment']['plot']
     mosaic = config_plot['mosaic']
     figsize = config_plot['figsize']
     fig, axd = plt.subplot_mosaic(mosaic, figsize=figsize)
     
-    # plot_with_errorbar(axd['score'], score, axis=0)
-    # axd['score'].set_title('score')
-    
-    
+    fig.suptitle(name)
+
+    # Plot metrics designated in config file.
     df = pd.DataFrame(session_metrics_history)
     df_arr = np.array(df.values.tolist())
     for k in list(np.array(mosaic).reshape(-1)):
         i = list(df.columns).index(k)
-        plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0)
+        # plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0, method='minmax')
+        # plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0, method='minmax', plot_highlight='max-100')
+        plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0, **config['experiment']['plot'].get('plotargs', {}))
+        # plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0, method='std', plot_highlight='max-100')
+        # plot_with_errorbar(axd[k], df_arr[:,i,:], axis=0, method='std')
         
         if 'title' in config_plot['axes'][k]:
             title = config_plot['axes'][k]['title']
@@ -130,25 +163,6 @@ def main(config: dict, dir: str):
         if 'ylabel' in config_plot['axes'][k]:
             axd[k].set_ylabel(config_plot['axes'][k]['ylabel'])
 
-    # i = list(df.columns).index('undiscounted_reward')
-    # diff = df_arr[:,i,:] - score
-    # print(np.allclose(diff, np.zeros_like(diff)))
-
-    # nl = np.array(nl)
-    # nl.reshape((len(df), len(metrics_history.keys()), 3000))
-    # print(df)
-    # print(df_arr.shape)
-    # for k in list(df.columns):
-    #     print(k)
-    #     print(df[k])
-    #     print(df.values.ravel())
-    #     break
-    
-    
-    
-    # plt.plot(x, score_mean, 'b-', linewidth=0.2)
-    # plt.fill_between(x, score_mean - score_std, score_mean+score_std, color='b', alpha=0.2, linewidth=0.2)
-    
     fig.tight_layout()
     plt.show()
 
@@ -170,6 +184,7 @@ def main(config: dict, dir: str):
 
 
 if __name__ == '__main__':
+
     # Get program options.
     opts = get_opts()
     
@@ -181,6 +196,8 @@ if __name__ == '__main__':
 
     # Run the experiment.
     main(
+        name=config_path.name,
         config=config,
-        dir=opts.dir,
+        files=opts.files,
+        flag_sort_files=opts.sort
     )

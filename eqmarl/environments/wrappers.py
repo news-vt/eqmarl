4cd55f53281f5a8010578107b75ca2653651395c
Alexander DeRieux <alexander.derieux@gmail.com>
2024-11-21 17:28:33 -0500

    Ensures unwrapped env agent position is a tuple type

--- eqmarl/environments/wrappers.py
+++ eqmarl/environments/wrappers.py
@@ -11,47 +11,22 @@ class StepRewardWrapper(gym.Wrapper):
     def step(self, action):
         # Take the given action.
         obs, reward, terminated, truncated, info = self.env.step(action)
-        
-        # # Compute slope to goal.
-        # slope = np.divide(
-        #     self.goal_position[1] - self.unwrapped.agent_pos[1],
-        #     self.goal_position[0] - self.unwrapped.agent_pos[0],
-        # )
-        # slope = np.arctan(slope) # Angle.
-        # # print(f"{slope=}, {np.arctan(slope)=}")
+
+        # Ensure the environment agent position is a tuple type.
+        current_pos = tuple(self.unwrapped.agent_pos)
 
         # Give a negative reward for standing still.
-        if not terminated and self.last_pos is not None and self.unwrapped.agent_pos == self.last_pos:
+        if not terminated and self.last_pos is not None and current_pos == self.last_pos:
             # print(f"matched pos")
             reward = -2.
         # Give a negative reward for each step.
         elif not terminated and reward == 0:
-                # reward = +1. * 1./slope
-                # reward = +1.
                 reward = -1.
         # If we're terminating, then scale the reward by 100.
         elif terminated:
             reward = reward * 100.
-            
-        self.last_pos = self.unwrapped.agent_pos
-        # print(f"{self.unwrapped.agent_pos=}")
+        
+        # Update agent position.
+        self.last_pos = current_pos
 
         return obs, reward, terminated, truncated, info
-    
-    # def reset(self, *args, **kwargs):
-    #     obs, info = self.env.reset(*args, **kwargs)
-
-    #     if not self.goal_position:
-    #         self.goal_position = [
-    #             x for x, y in enumerate(self.unwrapped.grid.grid) if isinstance(y, Goal)
-    #         ]
-    #         # in case there are multiple goals , needs to be handled for other env types
-    #         if len(self.goal_position) >= 1:
-    #             self.goal_position = (
-    #                 int(self.goal_position[0] / self.unwrapped.height),
-    #                 self.goal_position[0] % self.unwrapped.width,
-    #             )
-        
-    #     # print(f"{self.goal_position=}, {self.unwrapped.agent_pos=}")
-
-    #     return obs, info
